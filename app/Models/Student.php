d3145619134e44e1f1c54a1da72afb63e5fac386
Luys Fernnando <luysfernnando@gmail.com>
2024-11-13 07:42:26 -0300

    Created models for students, courses, and registrations tables

---
+++ app/Models/Student.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'birth_date'
+    ];
+
+    public function registrations()
+    {
+        return $this->hasMany(Registration::class);
+    }
+}
